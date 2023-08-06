c4aac77bfb4cf5bfea283077b8015daa5a4d603e
Arman <armangrigorian.work@gmail.com>
2023-08-06 15:47:21 +0400

    few fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ import Article from "./components/article/Article.jsx";
 import SubscribeForm from "./components/subscribeForm/SubscribeForm.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Aside from "./components/aside/Aside.jsx";
-import { dressesList, gymwearList, jacketList, jeansList, shirtsList, shoesList } from "./_DATA/index.js";
+import {
+	dressesList,
+	gymwearList,
+	jacketList,
+	jeansList,
+	shirtsList,
+	shoesList,
+} from "./_DATA/index.js";
 
 export default class App extends Component {
 	constructor(props) {
@@ -14,7 +21,14 @@ export default class App extends Component {
 		this.state = {
 			products: null,
 			title: "Welcome",
+			asideIsOpen: false,
 		};
+
+		this.handleAside = function () {
+			this.state.asideIsOpen === true
+				? this.setState({ asideIsOpen: false })
+				: this.setState({ asideIsOpen: true });
+		}.bind(this);
 
 		this.handleClick = function (e) {
 			switch (e.target.textContent) {
@@ -47,7 +61,12 @@ export default class App extends Component {
 			<>
 				<Aside handleClick={this.handleClick} />
 				<div className="mainContainer">
-					<Header text={this.state.title} />
+					<Header
+						text={this.state.title}
+						handleClick={this.handleClick}
+						handleAside={this.handleAside}
+						asideIsOpen={this.state.asideIsOpen}
+					/>
 					<Section />
 					{this.state.products && <Article products={this.state.products} />}
 					<SubscribeForm />
