144cf0e80afb5f1be5bf08d87c8354957155ca9d
Arman <armangrigorian.work@gmail.com>
2023-08-06 15:47:21 +0400

    few fixes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,13 @@ import { Component } from "react";
 import "./Header.scss";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MenuIcon from "@mui/icons-material/Menu";
 import PropTypes from "prop-types";
+import Aside from "../aside/Aside.jsx";
 
 export default class Header extends Component {
 	constructor(props) {
-		super(props)
-
+		super(props);
 	}
 
 	render() {
@@ -15,14 +16,19 @@ export default class Header extends Component {
 			<header>
 				<h3>{this.props.text}</h3>
 				<div>
+					<MenuIcon onClick={this.props.handleAside} />
 					<ShoppingCartIcon />
 					<SearchIcon />
 				</div>
+				{this.props.asideIsOpen ? <Aside handleClick={this.props.handleClick} /> : null}
 			</header>
 		);
 	}
 }
 
 Header.propTypes = {
-	text: PropTypes.string
-}
+	text: PropTypes.string,
+	handleClick: PropTypes.func,
+	handleAside: PropTypes.func,
+	asideIsOpen: PropTypes.bool,
+};
